d57e01bf8321cd007b5a42e6917938cdab01a77b
unknown <fernandocuestas886@gmail.com>
2019-11-25 01:41:31 -0300

    first commit

--- src/components/ContactPage/Contact.js
+++ src/components/ContactPage/Contact.js
@@ -8,7 +8,7 @@ export default function Contact() {
           <Title title="contact us" />
           <form
             className="mt-5"
-            action="https://formspree.io/learncodetutorial@gmail.com"
+            action=""
             method="POST"
           >
             {/* first */}
@@ -17,7 +17,7 @@ export default function Contact() {
                 type="text"
                 name="firstName"
                 className="form-control"
-                placeholder="john smith"
+                placeholder="Fernando Cuestas"
               />
             </div>
             {/* email */}
@@ -44,7 +44,7 @@ export default function Contact() {
                 name="message"
                 className="form-control"
                 rows="10"
-                placeholder="hello there buddy"
+                placeholder="hello"
               />
             </div>
             {/* submit */}
